23a166f0db49bec3ceab8c8a4cfb216aa77978c0
rockmel03 <kamalswag2@gmail.com>
2024-03-12 12:02:50 +0530

    created some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import React from "react";
-export default function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/reducers/AuthSlice";
+import { Footer, Header } from "./components";
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) =>
+        userData ? dispatch(login(userData)) : dispatch(logout())
+      )
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="w-screen h-screen bg-zinc-950 text-white overflow-y-auto">
+      <div className="w-full h-full flex flex-col justify-between">
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
